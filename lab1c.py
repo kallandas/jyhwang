a8595572ebfbe174f05d7827cb8ca6ecb3493612
Ji Yong Hwang <54117215+kallandas@users.noreply.github.com>
2022-09-12 12:10:27 -0400

    Lab1 results
    works from Lab1 final results. Total of 7 files.

---
+++ lab1c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Description: This program will output "Isaac is 72 years old!" to the screen.'''
+
+# Any line that starts with a "#" is also known as a comment,
+# these lines are ignored by the python interpreter even if
+# they contain code. The very first line is called a SheBang line,
+# it is used to tell the system which interpreter to
+# use(python2, python3, bash, etc).
+
+name = 'Isaac'
+age = 72
+print(name + ' is ' + str(age) + ' years old!')
---
+++ lab1d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Description: This program will output "10 + 2 * 5 = 20" to the screen.'''
+
+# Any line that starts with a "#" is also known as a comment,
+# these lines are ignored by the python interpreter even if
+# they contain code. The very first line is called a SheBang line,
+# it is used to tell the system which interpreter to
+# use(python2, python3, bash, etc).
+
+x = 10
+y = 2
+z = 5
+result = x + y * z
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
