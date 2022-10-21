f062a4b17a7ddf5aad4ae8a817b648214f9b7b5e
kallandas <kallandas76@gmail.com>
2022-10-20 23:00:47 -0400

    Milestone 2 tested.

--- assign_text.py
+++ assign_text.py
@@ -8,16 +8,9 @@ import subprocess
 def percent_to_graph(percent, max_length=20):
     # "returns a string: eg. '##  ' for 50 if total_chars == 4"  
     num_chr = round((max_length * percent) / 100) # Calculating input into actual number of characters to draw. Round up.
-    inv_pcnt = 100 - percent  # Calculating Inverse percent.
-    num_spc = round((max_length * inv_pcnt) / 100) # Calculating the number of spaces to print
+    num_spc = max_length - num_chr  # Calculating the number of spaces to print
     graph_draw = (('=' * num_chr) + (' ' * num_spc)) # print out the character "#" as many times as num_char value.
-    return num_chr, num_spc, graph_draw
-
-print(percent_to_graph(33,10))
-print(percent_to_graph(56,15))
-print(percent_to_graph(70,20))
-print(percent_to_graph(63,30))
-print(percent_to_graph(89,80))
+    return graph_draw
 
 def call_du_sub(location):
     p = subprocess.Popen(['du -d 1 ' + location], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -27,3 +20,25 @@ def call_du_sub(location):
     result = output[0].decode('utf-8').strip().split('\n')
     # Splits the tuple data into a list so that it can be used later on by another function.
     return result
+
+def create_dir_dict(raw_dat):
+    "get list from du_sub, return dict {'directory': 0} where 0 is size"
+    dict = {}
+    for item in raw_dat:
+        newlist = item.split('\t')
+        dict[newlist[1]] = newlist[0]
+    return dict
+
+def get_total(raw_dict, target_dir):
+    value = int()
+    for k, v in raw_dict.items():
+        if k == target_dir:
+            value = v 
+    return value
+
+    "return total size (in bytes) of the target directory"
+
+raw_dict = create_dir_dict(call_du_sub('~/ops435'))
+print(raw_dict)
+print(get_total(raw_dict, '/home/jyhwang/ops435/lab3'))
+
