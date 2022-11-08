3346ce8b5e66035f93cc774421b7aa70e9f6e1f2
kallandas <kallandas76@gmail.com>
2022-11-08 09:19:57 -0500

    Part 2. lab7b.py codes complete

--- lab7/lab7b.py
+++ lab7/lab7b.py
@@ -38,3 +38,20 @@ def valid_time(t):
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
        return False
     return True
+
+def change_time(time, seconds):
+    time.second += seconds
+    if valid_time(time) != True:
+        while time.second >= 60:
+            time.second -= 60
+            time.minute +=1
+        while time.minute >= 60:
+            time.minute -= 60
+            time.hour += 1
+        while time.second < 0:
+            time.minute -= 1
+            time.second += 60
+        while time.minute < 0:
+            time.hour -= 1
+            time.minute += 60
+    return None
