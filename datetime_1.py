0ed7345d4b48b6dcb8dd9c8d3e1fcea626cd1342
kallandas <kallandas76@gmail.com>
2022-12-01 20:50:43 -0500

    Minor fix

--- datetime_1.py
+++ datetime_1.py
@@ -1,7 +1,6 @@
 import sys
 from datetime import UTC, datetime
 
-
 date1 = "Jul/10/2019"
 datemask = "%b/%d/%Y"
 datetime_object1 = datetime.strptime(date1, datemask)
@@ -22,13 +21,13 @@ print(datetime_object3)
 print(myyear)
 
 dt = datetime(2016, 7, 27, 12, 30, 45, tzinfo=UTC)
-print("1: " + dt)
+print("1: " + str(dt))
 
-dt_now = datetime.datetime.now(tz=pytz.UTC)
-print("2: " + dt_now)
+dt_now = datetime.now(tz=UTC)
+print("2: " + str(dt_now))
 
-dt_utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
-print("3: " + dt_utcnow)
+dt_utcnow = datetime.utcnow().replace(tzinfo=UTC)
+print("3: " + str(dt_utcnow))
 
 date = "2018-13-10"
 
@@ -38,6 +37,3 @@ try:
 except ValueError:
     print('Date not recognized. Use YYYY-MM-DD format.')
     sys.exit()
-
-
-
