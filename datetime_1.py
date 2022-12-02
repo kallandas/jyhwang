401d67f58ae00a37d3e91094c96be3952381811a
kallandas <kallandas76@gmail.com>
2022-12-01 20:44:46 -0500

    fixed file name

--- datetime_1.py
+++ datetime_1.py
@@ -1,5 +1,6 @@
 import sys
-from datetime import datetime
+from datetime import UTC, datetime
+
 
 date1 = "Jul/10/2019"
 datemask = "%b/%d/%Y"
@@ -10,17 +11,33 @@ mydate = datetime.strftime(datetime_object1,'%Y-%m-%d')
 myyear = datetime.strftime(datetime_object1,'%Y')
 tempdate1 = datetime.strptime('Sun 20 Jul 18:00:00 2018', '%a %d %b %H:%M:%S %Y')
 tempdate2 = datetime.strptime('2018-07-21', '%Y-%m-%d')
+tempdate3 = datetime.strftime(tempdate1, '%H:%M:%S')
 print(tempdate1 > tempdate2)
 print(tempdate1)
+print(tempdate3)
 print(mydate)
 print(datetime_object1)
 print(datetime_object2)
 print(datetime_object3)
 print(myyear)
+
+dt = datetime(2016, 7, 27, 12, 30, 45, tzinfo=UTC)
+print("1: " + dt)
+
+dt_now = datetime.datetime.now(tz=pytz.UTC)
+print("2: " + dt_now)
+
+dt_utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+print("3: " + dt_utcnow)
+
 date = "2018-13-10"
+
 try:
     dateInput = datetime.strptime(date, '%Y-%m-%d')
     print(dateInput)
 except ValueError:
     print('Date not recognized. Use YYYY-MM-DD format.')
     sys.exit()
+
+
+
